8cf21e4b6ee5f61e3fb9e939a30f4a335aec85dd
Shahd <shahdalnomani2004@gmail.com>
2025-06-16 10:42:38 +0300

    fix: resolved merge conflicts and modified files

--- src/main.jsx
+++ src/main.jsx
@@ -1,25 +1,10 @@
-<<<<<<< HEAD
-// src/main.jsx
-
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
-import './index.css';
+import App from './App';
+import './App.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>
 );
-=======
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
->>>>>>> 516b076534b87358c005c4c6ae7956a22f28891d
