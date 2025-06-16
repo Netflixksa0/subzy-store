bce6be9526f65116c2f6ced30b905643f24ad1ec
Shahd <shahdalnomani2004@gmail.com>
2025-06-16 07:32:29 +0300

    إصلاح تعارض vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,7 @@
-<<<<<<< HEAD
-// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-export default {
-  server: {
-    host: true
-  }
-}
-=======
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
->>>>>>> 516b076534b87358c005c4c6ae7956a22f28891d
+});
